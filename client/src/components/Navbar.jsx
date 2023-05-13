8914a39271fad080e6beee2fd74cdc01858e07c5
Kushal Goyal <goyalkushal04@gmail.com>
2023-05-13 10:31:23 +0530

    added login, signup, navbar components and a bunch of css classes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faUserPlus, faPersonChalkboard, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+const categories = [
+    'Computer Science',
+    'Business',
+    'Art & Design',
+    'Social Sciences',
+    'Health & Medicine',
+    'Mathematics',
+    'Physical Science & Engineering',
+    'Data Science',
+    'Language Learning',
+    'Personal Development'
+];
 
 const Navbar = (props) => {
 
@@ -12,9 +26,25 @@ const Navbar = (props) => {
 
     return (
         <>
-        <h1 className='primary-heading'>Navbar</h1>
-        <button onClick={handleSignup}>Signup</button>
-        <button onClick={handleLogin}>Login</button>
+            <div className="flex justify-space-between align-center padding primary-bgcolor  ">
+                <h1 className='primary-heading'><FontAwesomeIcon icon={faPersonChalkboard} style={{color: "#7080ee",}} /> [name]</h1>
+                <div className="SearchBoxContainer">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                <input type="text" className='search-input' placeholder='Search for cources' />
+                </div>
+                <div className='dropDown'>
+                    <button className='dropDown-btn heading hover-type-underline'>Categories</button>
+                    <ul className='dropDown-content'>
+                        {categories.map((category, index) => (
+                            <li key={index}>{category}</li>
+                        ))}
+                    </ul>
+                </div>
+                <span className='tertiary-heading pointer hover-type-underline'>Teach on [name]</span>
+                <FontAwesomeIcon icon={faCartShopping} className='cart'/>
+                <button className='btn-type-1 heading btn hover-type-scale'>Log in</button>
+                <button className='btn-type-2 heading btn hover-type-scale'>Sign up</button>
+            </div>
         </>
     )
 }
